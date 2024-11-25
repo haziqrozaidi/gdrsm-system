cc7d1dbe8e5f536d8b62e11a751ada89dfc85d94
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2024-11-26 00:33:46 +0800

    Initialize frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import 'primeicons/primeicons.css'
+import './style.css'
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+
+import App from './App.vue'
+import Noir from './presets/Noir.js';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Noir
+    }
+});
+
+app.mount('#app')
