1c1a1282b49425d9d9a25005fce1a48b4e0a4a1b
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2024-11-30 22:55:25 +0800

    Add sidebar component

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,61 @@
+<script setup>
+    import Menu from 'primevue/menu';
+    import { ref } from 'vue';
+
+    const items = ref([
+        {
+            label: 'Dashboard',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Shared Resources',
+            items: [
+                {
+                    label: 'Files',
+                    icon: 'pi pi-file'
+                },
+                {
+                    label: 'Folders',
+                    icon: 'pi pi-folder'
+                },
+                {
+                    label: 'Sharing',
+                    icon: 'pi pi-users'
+                }
+            ]
+        },
+        {
+            label: 'Documents',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-plus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-search'
+                }
+            ]
+        },
+        {
+            label: 'Profile',
+            items: [
+                {
+                    label: 'Settings',
+                    icon: 'pi pi-cog'
+                },
+                {
+                    label: 'Logout',
+                    icon: 'pi pi-sign-out'
+                }
+            ]
+        }
+    ]);
+</script>
+
+<template>
+    <Menu :model="items" />
+</template>
+
+<style scoped>
+</style>
